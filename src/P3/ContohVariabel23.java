56da46e77a3a320b259540d90bd146f660538e7e
Zephyrif <rifdamusyaffa007@gmail.com>
2023-09-15 14:02:29 +0700

    P3_percobaan1_Pertanyaan1

--- src/P3/ContohVariabel23.java
+++ src/P3/ContohVariabel23.java
@@ -2,12 +2,12 @@ package P3;
 
 public class ContohVariabel23 {
     public static void main(String[] args) {
-        String salahSatuHobySayaAdalah = "Bermain petak umpet";
+        String hobySaya = "Bermain petak umpet";
         boolean isPandai = true;
         char jenisKelamin= 'L';
         byte umurSayaSekarang = 20;
         double $ipk = 3.24, tinggi = 1.78;
-        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println(hobySaya);
         System.out.println("Apakah pandai? " + isPandai);
         System.out.println("Jenis kelamin: " + jenisKelamin);
         System.out.println("Umurku saat ini: " + umurSayaSekarang);
