ca693af5193db935269432fa2ef033b7beb869a2
Zephyrif <rifdamusyaffa007@gmail.com>
2023-09-22 14:38:00 +0700

    P4_Percobaan1_23

--- src/P4/Lingkaran23.java
+++ src/P4/Lingkaran23.java
@@ -7,9 +7,9 @@ public class Lingkaran23 {
         int r;
         double keliling, luas;
         System.out.println("Masukkan jari-jari lingkaran: ");
-        r= input23.nextInt();
-        keliling= 2*3.14*r;
-        luas= 3.14*r*r;
+        r = input23.nextInt();
+        keliling = 2*3.14*r;
+        luas = 3.14*r*r;
         input23.close();
         System.out.println(keliling);
         System.out.println(luas);
