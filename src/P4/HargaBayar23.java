0e387b3b1f82f8d6e2f95cc3ef4cd2da13ed674b
Zephyrif <rifdamusyaffa007@gmail.com>
2023-09-22 17:42:25 +0700

    P4_Percobaan3_Pertanyaan2_23

--- src/P4/HargaBayar23.java
+++ src/P4/HargaBayar23.java
@@ -4,16 +4,25 @@ import java.util.Scanner;
 public class HargaBayar23 {
     public static void main(String[] args) {
         Scanner input23 = new Scanner(System.in);
-        int harga, jumlah;
-        double dis = 0.1, total, bayar, jmlDis;
+        int harga, jmlBarang, jmlHalBuku;
+        double dis, total, bayar, jmlDis;
+        String merk;
         System.out.println("Masukkan harga barang ");
         harga = input23.nextInt();
         System.out.println("Masukkan jumlah barang ");
-        jumlah = input23.nextInt();
-        total = harga*jumlah;
+        jmlBarang = input23.nextInt();
+        System.out.println("Masukkan merk buku");
+        merk = input23.next();
+        System.out.println("Masukkan jumlah halaman buku");
+        jmlHalBuku = input23.nextInt();
+        System.out.println("Masukkan diskon");
+        dis = input23.nextDouble();
+        total = harga*jmlBarang;
         jmlDis = total*dis;
         bayar = total- jmlDis;
         input23.close();
+        System.out.println("Merk buku: " + merk);
+        System.out.println("jumlah halaman buku: " + jmlHalBuku);
         System.out.println("Diskon anda adalah " + jmlDis);
         System.out.println("jumlah yang harus dibayar adalah " + bayar);
 
