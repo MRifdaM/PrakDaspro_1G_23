6bd90ca026c15efb0e92475f79efa9d9e342c37a
Zephyrif <rifdamusyaffa007@gmail.com>
2023-09-22 15:44:55 +0700

    P4_Percobaan2_Pertanyaan2_23

--- src/P4/Gaji23.java
+++ src/P4/Gaji23.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 public class Gaji23 {
     public static void main(String[] args){
         Scanner input23 = new Scanner(System.in);
-        int jmlMasuk, jmlTdkMasuk, totGaji;
-        int gaji=40000, potGaji=25000;
+        int jmlMasuk, jmlTdkMasuk, totGaji, gaji, potGaji;
         System.out.println("Masukkan jumlah hari masuk anda ");
         jmlMasuk = input23.nextInt();
         System.out.println("Masukkan jumlah hari tidak masuk anda ");
         jmlTdkMasuk = input23.nextInt();
+        System.out.println("Masukkan gaji anda ");
+        gaji = input23.nextInt();
+        System.out.println("Masukkan potongan gaji anda ");
+        potGaji = input23.nextInt();
         totGaji = (jmlMasuk*gaji)-(jmlTdkMasuk*potGaji);
         input23.close();
         System.out.println("Gaji anda adalah " + totGaji);
