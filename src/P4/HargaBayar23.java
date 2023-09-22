d3f2b2b5c6104e8af006bc8bcf6f3cbecfd76970
Zephyrif <rifdamusyaffa007@gmail.com>
2023-09-22 16:06:06 +0700

    P4_Percobaan3_23

---
+++ src/P4/HargaBayar23.java
@@ -0,0 +1,20 @@
+package P4;
+import java.util.Scanner;
+
+public class HargaBayar23 {
+    public static void main(String[] args) {
+        Scanner input23 = new Scanner(System.in);
+        int harga, jumlah;
+        double dis = 0.1, total, bayar, jmlDis;
+        System.out.println("Masukkan harga barang ");
+        harga = input23.nextInt();
+        System.out.println("Masukkan jumlah barang ");
+        jumlah = input23.nextInt();
+        total = harga*jumlah;
+        jmlDis = total*dis;
+        bayar = total- jmlDis;
+        System.out.println("Diskon anda adalah " + jmlDis);
+        System.out.println("jumlah yang harus dibayar adalah " + bayar);
+
+    }
+}
