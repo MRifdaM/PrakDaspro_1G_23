a26ed5e49ad1619d7a4b840b50a35a196f2ff63f
Zephyrif <rifdamusyaffa007@gmail.com>
2023-10-11 19:59:45 +0700

    P6_Percobaan3_Pertanyaan3_23

--- src/P6/Pemilihan2Percobaan323.java
+++ src/P6/Pemilihan2Percobaan323.java
@@ -12,7 +12,7 @@ public class Pemilihan2Percobaan323 {
         System.out.println("Masukkan Besarnya penghasilan: ");
         penghasilan=input23.nextInt();
         input23.close();
-        if (kategori.equalsIgnoreCase("pekerja")){
+        if (kategori.equals("pekerja")){
             if (penghasilan<=2000000){
                 pajak = 0.1;
             } else if (penghasilan <= 3000000) {
@@ -22,7 +22,7 @@ public class Pemilihan2Percobaan323 {
                 gajiBersih=(int)(penghasilan-(pajak*penghasilan));
                 System.out.println("Penghasilan Bersih: " + gajiBersih);
             
-    } else if (kategori.equalsIgnoreCase("pebisnis")){
+    } else if (kategori.equals("pebisnis")){
         if(penghasilan<=2500000) {
             pajak= 0.15;
         } else if (penghasilan <= 3500000) {
