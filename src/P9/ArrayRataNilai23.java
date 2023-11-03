dec2223d3e894d6058b8b14bd253d10b0a2f4e8e
Zephyrif <rifdamusyaffa007@gmail.com>
2023-11-03 14:54:29 +0700

    P9_Percobaan3_Pertanyaan1_23

--- src/P9/ArrayRataNilai23.java
+++ src/P9/ArrayRataNilai23.java
@@ -5,6 +5,7 @@ public class ArrayRataNilai23 {
     public static void main(String[] args) {
     Scanner sc23 = new Scanner(System.in);
     int[] nilaiMhs = new int[10];
+    int jmlMahasiswa = 0;
     double total = 0;
     double rata2;
 
@@ -13,10 +14,16 @@ public class ArrayRataNilai23 {
         nilaiMhs[i] = sc23.nextInt();
     }
     for (int i = 0; i < nilaiMhs.length; i++) {
+        if (nilaiMhs[i] > 70) {
+            jmlMahasiswa++;
+        }
+    }
+    for (int i = 0; i < nilaiMhs.length; i++) {
         total += nilaiMhs[i];
     }
     rata2 = total/nilaiMhs.length;
     System.out.println("Rata-rata nilai = "+rata2);
+    System.out.println("Jumlah Mahasiswa yang lulus sebanyak " + jmlMahasiswa + " Mahasiswa");
     sc23.close();
     }
 }
