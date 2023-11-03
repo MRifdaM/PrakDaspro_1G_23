b7973d9fa0a2d480d842a3d72d331e29faa21945
Zephyrif <rifdamusyaffa007@gmail.com>
2023-11-03 13:36:30 +0700

    P9_Percobaan1_Pertanyaan1_23

--- src/P9/ArrayBilangan23.java
+++ src/P9/ArrayBilangan23.java
@@ -2,10 +2,14 @@ package P9;
 
 public class ArrayBilangan23 {
     public static void main(String[] args) {
-        int[] bil = {5, 13, -7, 17};
-
-        for (int i = 0; i < 4; i++) {
-            System.out.println(bil[i]);
-        }
+        int bil[] = new int[4];
+        bil[0] = 5.0;
+        bil[1] = 12867;
+        bil[2] = 7.5;
+        bil[3] = 2000000;
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
     }
 }
