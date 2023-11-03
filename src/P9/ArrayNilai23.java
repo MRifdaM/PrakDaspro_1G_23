92437e4bbca5181ef9d502842306398251cf2ff4
Zephyrif <rifdamusyaffa007@gmail.com>
2023-11-03 14:17:34 +0700

    P9_Percobaan2_Pertanyaan1_23

--- src/P9/ArrayNilai23.java
+++ src/P9/ArrayNilai23.java
@@ -5,7 +5,7 @@ public class ArrayNilai23 {
     public static void main(String[] args) {
         Scanner sc23 = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" +i+ " : ");
             nilaiAkhir[i] = sc23.nextInt();
         }
