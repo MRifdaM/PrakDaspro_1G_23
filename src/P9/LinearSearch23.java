87a34c691c0d190751f3852c4745ca0d86fd2aa1
Zephyrif <rifdamusyaffa007@gmail.com>
2023-11-03 16:03:46 +0700

    P9_Percobaan4_Pertanyaan2_23

--- src/P9/LinearSearch23.java
+++ src/P9/LinearSearch23.java
@@ -1,9 +1,20 @@
 package P9;
+import java.util.Scanner;
+
 public class LinearSearch23 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+       Scanner sc23 = new Scanner(System.in);
+       int n = 0, key = 0, hasil = 0;
+       System.out.print("Masukkan jumlah elemen array :");
+       n = sc23.nextInt();
+       int[] arrayInt = new int[n];
+
+       for (int i = 0; i < arrayInt.length; i++) {
+        System.out.print("Masukkan elemen array ke-"+i+ " :");
+        arrayInt[i] = sc23.nextInt();
+       }
+       System.out.println("Masukkan key yang ingin dicari: ");
+       key = sc23.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
@@ -11,6 +22,7 @@ public class LinearSearch23 {
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        System.out.println("Key ada di posisi indeks ke-"+hasil);
+        sc23.close();
     }
 }
