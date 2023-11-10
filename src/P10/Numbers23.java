e38ea453ad4ca3f282829a96d43a74a0592229c3
Zephyrif <rifdamusyaffa007@gmail.com>
2023-11-10 17:05:12 +0700

    P10_Percobaan3_Pertanyaan1_23

--- src/P10/Numbers23.java
+++ src/P10/Numbers23.java
@@ -1,5 +1,7 @@
 package P10;
 
+import java.util.Arrays;
+
 public class Numbers23 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
@@ -7,5 +9,8 @@ public class Numbers23 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
         
+       for(int i = 0; i < myNumbers.length; i++){
+        System.out.println(Arrays.toString(myNumbers[i]));
+       }
     }
 }
