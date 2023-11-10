c0cba2d63e09b8955cbeca87c8df9bb336e8ecb3
Zephyrif <rifdamusyaffa007@gmail.com>
2023-11-10 15:27:31 +0700

    P10_Percobaan2_Pertanyaan2_23

--- src/P10/BioskopWithScanner23.java
+++ src/P10/BioskopWithScanner23.java
@@ -5,25 +5,55 @@ import java.util.Scanner;
 public class BioskopWithScanner23 {
     public static void main(String[] args) {
         Scanner sc23 = new Scanner(System.in);
-        int baris, kolom;
-        String nama, next;
+        int baris, kolom, pilih;
+        String nama, next, pilih2;
+        boolean menu = true;
         String penonton[][] = new String[4][2];
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc23.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc23.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc23.nextInt();
-            sc23.nextLine();
-            penonton[baris-1][kolom-1] = nama;
-            System.out.print("Input penonton lainnya? (y/t): ");
-            next = sc23.nextLine();
-            if (next.equalsIgnoreCase("n")) {
-                break;
+        while (menu) {
+            System.out.println("MENU 1: INPUT DATA PENONTON");
+            System.out.println("MENU 2:TAMPILKAN DAFTAR PENONTON");
+            System.out.println("MENU 3: EXIT");
+            System.out.print("Masukkan menu : ");
+            pilih = sc23.nextInt();
+
+            switch (pilih) {
+                case 1:
+                while(true) {
+                    System.out.println("==INPUT DATA PENONTON==");
+                    System.out.print("Masukkan nama: ");
+                    nama = sc23.next();
+                    System.out.print("Masukkan baris: ");
+                    baris = sc23.nextInt();
+                    System.out.print("Masukkan kolom: ");
+                    kolom = sc23.nextInt();
+                    sc23.nextLine();
+                    penonton[baris - 1][kolom - 1] = nama;
+                    System.out.print("Input penonton lainnya? (y/n): ");
+                    next = sc23.next();
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }
+                    break;
+                case 2:
+                    System.out.println("==TAMPILKAN DAFTAR PENONTON==");
+                    for (int i = 0; i < penonton.length; i++) {
+                        System.out.println("Penonton pada baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
+                    }
+                    break;
+                case 3:
+                    System.out.println("==EXIT==");
+                    System.out.print("Keluar (y/t) ?: ");
+                    pilih2 = sc23.next();
+                    if (pilih2.equalsIgnoreCase("y")) {
+                        menu = false;
+                    } else {
+                        break;
+                    }
+                    break;
             }
-         }
-         sc23.close();
+        }
+        sc23.close();
     }
 }
