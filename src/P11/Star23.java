a9cb657fc8f6082409c7451a510c8f79472b9fc5
Zephyrif <rifdamusyaffa007@gmail.com>
2023-11-13 07:51:46 +0700

    P11_Percobaan1_23

---
+++ src/P11/Star23.java
@@ -0,0 +1,14 @@
+package P11;
+import java.util.Scanner;
+
+public class Star23 {
+    public static void main(String[] args) {
+        Scanner sc23 = new Scanner(System.in);
+        int n = 0;
+        System.out.print("Masukkan nilai N : ");
+        n = sc23.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+    }
+}
+}
