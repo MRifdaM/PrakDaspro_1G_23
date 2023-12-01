9df11142008493bb1a7fba68a93fb86559973fb6
Zephyrif <rifdamusyaffa007@gmail.com>
2023-12-01 13:58:17 +0700

    P13_Percobaan4_23

---
+++ src/P13/UcapanTerimaKasih_23.java
@@ -0,0 +1,21 @@
+package P13;
+import java.util.Scanner;
+
+public class UcapanTerimaKasih_23 {
+    public static String PenerimaUcapan(){
+        Scanner sc23 = new Scanner(System.in);
+        System.out.print("Tuliskan NAMA orang yang ingin anda beri ucapan : ");
+        String namaOrang = sc23.nextLine();
+        sc23.close();
+        return namaOrang;
+    }
+    public static void UcapanTerimaKasih(){
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n" +
+        "You inspired in me a love for learning and made me feel like i could ask you anything.");
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+}
