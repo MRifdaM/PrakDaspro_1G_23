050117b72c3f5cebcab0a30b86987d4d45f838af
Zephyrif <rifdamusyaffa007@gmail.com>
2023-12-08 14:10:34 +0700

    P14_Tugas1_23

---
+++ src/Rekursif/Tugas1_23.java
@@ -0,0 +1,33 @@
+package Rekursif;
+
+import java.util.Scanner;
+
+public class Tugas1_23 {
+    static void rekursif(int n) {
+        if (n <= 0) {
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n + ", ");
+        rekursif(n - 1);
+    }
+
+    static void iteratif(int n) {
+        for (int i = n; i >= 0; i--) {
+            System.out.print(i);
+            if (i != 0)
+                System.out.print(", ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int n = sc.nextInt();
+        System.out.println("Deret rekursif: ");
+        rekursif(n);
+        System.out.println("Deret iteratif: ");
+        iteratif(n);
+        sc.close();
+    }
+}
